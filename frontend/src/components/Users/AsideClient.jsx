d374f1245eb6dfa6bb512f22b323a85e7d8f014e
joachimbang <joachimbangirahe.jb@gmail.com>
2025-05-30 23:25:51 +0300

    creating asideclient component.

---
+++ frontend/src/components/Users/AsideClient.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const AsideClient = ({}) => {
+
+  return (
+    <aside className="w-64 bg-white shadow-md p-4 space-y-4">
+      <h2 className="text-2xl font-bold text-primary">Dashboard</h2>
+      <nav className="space-y-2">
+        <Link to="/menu" className="block hover:text-primary">
+          Liste pizza
+        </Link>
+        <Link to="/historic" className="block hover:text-primary">
+          Historics
+        </Link>
+      </nav>
+    </aside>
+
+    // </div>
+  );
+};
+
+export default AsideClient;
+
+
